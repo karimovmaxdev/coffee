6378d71b24d58228b5fe99d00576f53333a50535
Maxim <karimov.maxdev@gmail.com>
2024-03-05 01:49:22 +0500

    test upd

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SelectorView from '../views/SelectorView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
